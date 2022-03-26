ccc85f985c0914cb11126fe1eace52340017149f
Paul Leger <pleger@gmail.com>
2022-03-25 21:00:21 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  dataUrl: 'https://gist.githubusercontent.com/chelosky/31056037b0fc5858768758d4b199306e/raw/95f38e15bc6bc181b004be274989e4e47126441e/json',
+  dataUrl: 'https://gist.githubusercontent.com/pleger/c761cfb94bcdf10dee057dc4dfcd9536/raw/3923cd62ed2812bc4123e6ec90670fafe6360d75/json',
   evaluationFormUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSdvuYk4w-84rlmJg8ULssTi07mIQSNgCSi8_nhNLIrMVw4H0A/viewform'
 };
